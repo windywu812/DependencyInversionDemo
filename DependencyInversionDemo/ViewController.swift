4eeb949907401ca4ca099932481c664fa0dd2eaa
Windy <windywu812@gmail.com>
2021-10-13 13:04:05 +0700

    Initial Commit

---
+++ DependencyInversionDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DependencyInversionDemo
+//
+//  Created by Windy on 13/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
